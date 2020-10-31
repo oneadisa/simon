fbd9586016df53ff2e2d1dcdac502b8f70b4ab37
adisa-stack1 <71322125+adisa-stack1@users.noreply.github.com>
2020-10-31 18:15:05 +0100

    Update game.js

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
+$(document).on('keydown' function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
